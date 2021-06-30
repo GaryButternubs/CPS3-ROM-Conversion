38b96e3a99238ddd81b2734b73a1b2a6debe8b9a
GaryButternubs <62677471+GaryButternubs@users.noreply.github.com>
2021-06-29 20:49:17 -0400

    The Source Code of the Program

--- CombinedAndSplitSwap.java
+++ CombinedAndSplitSwap.java
@@ -68,17 +68,29 @@ public class CombinedAndSplitSwap
 
 class CombinedToSplit implements ActionListener
 {
-	final String[] gamesSupported = {"JoJo's HFTF", "Third Strike"};
+	final String[] gamesSupported = {"JoJo's HFTF", "JoJo's Venture", "Third Strike", "Second Impact", "New Generation", "Red Earth"};
 
-	final String[][] combinedSet = {{"10", "20", "30", "31", "40", "41", "50", "51"}, //JoJo's HFTF
-			{"10", "20", "30", "31", "40", "41", "50", "51", "60", "61"}}; //Third Strike
+	final String[][] combinedSet = {
+			{"10", "20", "30", "31", "40", "41", "50", "51"}, //JoJo's HFTF
+			{"10", "20", "30", "31", "40", "41", "50"}, //JoJo's Venture
+			{"10", "20", "30", "31", "40", "41", "50", "51", "60", "61"}, //Third Strike
+			{"10", "20", "30", "31", "40", "41", "50", "51"}, //Second Impact
+			{"10", "30", "31", "40", "41", "50"}, //New Generation
+			{"10", "30", "31", "40", "41", "50"} //Red Earth
+	};
 
-	final String[][] splitSet = {{"jojoba-simm1.0", "jojoba-simm1.1", "jojoba-simm1.2", "jojoba-simm1.3", "jojoba-simm2.0", "jojoba-simm2.1", "jojoba-simm2.2", "jojoba-simm2.3", "jojoba-simm3.0", "jojoba-simm3.1", "jojoba-simm3.2", "jojoba-simm3.3", "jojoba-simm3.4", "jojoba-simm3.5", "jojoba-simm3.6", "jojoba-simm3.7", "jojoba-simm4.0", "jojoba-simm4.1", "jojoba-simm4.2", "jojoba-simm4.3", "jojoba-simm4.4", "jojoba-simm4.5", "jojoba-simm4.6", "jojoba-simm4.7", "jojoba-simm5.0", "jojoba-simm5.1", "jojoba-simm5.2", "jojoba-simm5.3", "jojoba-simm5.4", "jojoba-simm5.5", "jojoba-simm5.6", "jojoba-simm5.7"}, //JoJo's HFTF
-			{"sfiii3-simm1.0", "sfiii3-simm1.1", "sfiii3-simm1.2", "sfiii3-simm1.3", "sfiii3-simm2.0", "sfiii3-simm2.1", "sfiii3-simm2.2", "sfiii3-simm2.3", "sfiii3-simm3.0", "sfiii3-simm3.1", "sfiii3-simm3.2", "sfiii3-simm3.3", "sfiii3-simm3.4", "sfiii3-simm3.5", "sfiii3-simm3.6", "sfiii3-simm3.7", "sfiii3-simm4.0", "sfiii3-simm4.1", "sfiii3-simm4.2", "sfiii3-simm4.3", "sfiii3-simm4.4", "sfiii3-simm4.5", "sfiii3-simm4.6", "sfiii3-simm4.7", "sfiii3-simm5.0", "sfiii3-simm5.1", "sfiii3-simm5.2", "sfiii3-simm5.3", "sfiii3-simm5.4", "sfiii3-simm5.5", "sfiii3-simm5.6", "sfiii3-simm5.7", "sfiii3-simm6.0", "sfiii3-simm6.1", "sfiii3-simm6.2", "sfiii3-simm6.3", "sfiii3-simm6.4", "sfiii3-simm6.5", "sfiii3-simm6.6", "sfiii3-simm6.7"}}; //Third Strike
+	final String[][][] splitSet = {
+			{{"jojoba-simm1.0", "jojoba-simm1.1", "jojoba-simm1.2", "jojoba-simm1.3"}, {"jojoba-simm2.0", "jojoba-simm2.1", "jojoba-simm2.2", "jojoba-simm2.3"}, {"jojoba-simm3.0", "jojoba-simm3.1", "jojoba-simm3.2", "jojoba-simm3.3"}, {"jojoba-simm3.4", "jojoba-simm3.5", "jojoba-simm3.6", "jojoba-simm3.7"}, {"jojoba-simm4.0", "jojoba-simm4.1", "jojoba-simm4.2", "jojoba-simm4.3"}, {"jojoba-simm4.4", "jojoba-simm4.5", "jojoba-simm4.6", "jojoba-simm4.7"}, {"jojoba-simm5.0", "jojoba-simm5.1", "jojoba-simm5.2", "jojoba-simm5.3"}, {"jojoba-simm5.4", "jojoba-simm5.5", "jojoba-simm5.6", "jojoba-simm5.7"}}, //JoJo's HFTF
+			{{"jojo-simm-1.0", "jojo-simm-1.1", "jojo-simm-1.2", "jojo-simm-1.3"}, {"jojo-simm-2.0", "jojo-simm-2.1", "jojo-simm-2.2", "jojo-simm-2.3"}, {"jojo-simm-3.0", "jojo-simm-3.1", "jojo-simm-3.2", "jojo-simm-3.3"}, {"jojo-simm-3.4", "jojo-simm-3.5", "jojo-simm-3.6", "jojo-simm-3.7"}, {"jojo-simm-4.0", "jojo-simm-4.1", "jojo-simm-4.2", "jojo-simm-4.3"}, {"jojo-simm-4.4", "jojo-simm-4.5", "jojo-simm-4.6", "jojo-simm-4.7"}, {"jojo-simm-5.0", "jojo-simm-5.1"}}, //JoJo's Venture
+			{{"sfiii3-simm1.0", "sfiii3-simm1.1", "sfiii3-simm1.2", "sfiii3-simm1.3"}, {"sfiii3-simm2.0", "sfiii3-simm2.1", "sfiii3-simm2.2", "sfiii3-simm2.3"}, {"sfiii3-simm3.0", "sfiii3-simm3.1", "sfiii3-simm3.2", "sfiii3-simm3.3"}, {"sfiii3-simm3.4", "sfiii3-simm3.5", "sfiii3-simm3.6", "sfiii3-simm3.7"}, {"sfiii3-simm4.0", "sfiii3-simm4.1", "sfiii3-simm4.2", "sfiii3-simm4.3"}, {"sfiii3-simm4.4", "sfiii3-simm4.5", "sfiii3-simm4.6", "sfiii3-simm4.7"}, {"sfiii3-simm5.0", "sfiii3-simm5.1", "sfiii3-simm5.2", "sfiii3-simm5.3"}, {"sfiii3-simm5.4", "sfiii3-simm5.5", "sfiii3-simm5.6", "sfiii3-simm5.7"}, {"sfiii3-simm6.0", "sfiii3-simm6.1", "sfiii3-simm6.2", "sfiii3-simm6.3"}, {"sfiii3-simm6.4", "sfiii3-simm6.5", "sfiii3-simm6.6", "sfiii3-simm6.7"}}, //Third Strike
+			{{"sfiii2-simm1.0", "sfiii2-simm1.1", "sfiii2-simm1.2", "sfiii2-simm1.3"}, {"sfiii2-simm2.0", "sfiii2-simm2.1", "sfiii2-simm2.2", "sfiii2-simm2.3"}, {"sfiii2-simm3.0", "sfiii2-simm3.1", "sfiii2-simm3.2", "sfiii2-simm3.3"}, {"sfiii2-simm3.4", "sfiii2-simm3.5", "sfiii2-simm3.6", "sfiii2-simm3.7"}, {"sfiii2-simm4.0", "sfiii2-simm4.1", "sfiii2-simm4.2", "sfiii2-simm4.3"}, {"sfiii2-simm4.4", "sfiii2-simm4.5", "sfiii2-simm4.6", "sfiii2-simm4.7"}, {"sfiii2-simm5.0", "sfiii2-simm5.1", "sfiii2-simm5.2", "sfiii2-simm5.3"}, {"sfiii2-simm5.4", "sfiii2-simm5.5", "sfiii2-simm5.6", "sfiii2-simm5.7"}}, //Second Impact
+			{{"sfiii-simm1.0", "sfiii-simm1.1", "sfiii-simm1.2", "sfiii-simm1.3"}, {"sfiii-simm3.0", "sfiii-simm3.1", "sfiii-simm3.2", "sfiii-simm3.3"}, {"sfiii-simm3.4", "sfiii-simm3.5", "sfiii-simm3.6", "sfiii-simm3.7"}, {"sfiii-simm4.0", "sfiii-simm4.1", "sfiii-simm4.2", "sfiii-simm4.3"}, {"sfiii-simm4.4", "sfiii-simm4.5", "sfiii-simm4.6", "sfiii-simm4.7"}, {"sfiii-simm5.0", "sfiii-simm5.1"}}, //New Generation
+			{{"redearth-simm1.0", "redearth-simm1.1", "redearth-simm1.2", "redearth-simm1.3"}, {"redearth-simm3.0", "redearth-simm3.1", "redearth-simm3.2", "redearth-simm3.3"}, {"redearth-simm3.4", "redearth-simm3.5", "redearth-simm3.6", "redearth-simm3.7"}, {"redearth-simm4.0", "redearth-simm4.1", "redearth-simm4.2", "redearth-simm4.3"}, {"redearth-simm4.4", "redearth-simm4.5", "redearth-simm4.6", "redearth-simm4.7"}, {"redearth-simm5.0", "redearth-simm5.1"}} //Red Earth
+	};
 
 	String[] combined;
-	String[] split;
-	
+	String[][] split;
+
 	static JFileChooser chooser = new JFileChooser();
 
 	@SuppressWarnings("resource")
@@ -98,7 +110,7 @@ class CombinedToSplit implements ActionListener
 		{
 			String combinedPath = chooser.getSelectedFile().toString();
 
-			JOptionPane.showMessageDialog(null, "Please select the directory where you want to place the Split ROM. Make sure there are no jojoba-simm files there currently");
+			JOptionPane.showMessageDialog(null, "Please select the directory where you want to place the Split ROM. Make sure there are no simm files there currently");
 
 			returnVal = chooser.showOpenDialog(new JFrame());
 
@@ -113,14 +125,14 @@ class CombinedToSplit implements ActionListener
 					try 
 					{
 						if(a < 2)
-							fileDivide = splitDataFile(combinedPath + "/" + combined[a]);
+							fileDivide = splitDataFile(combinedPath + "/" + combined[a], split[a].length);
 
 						else
-							fileDivide = splitUserFile(combinedPath + "/" + combined[a]);
+							fileDivide = splitUserFile(combinedPath + "/" + combined[a], split[a].length);
 
-						for(int b = 0; b < 4; b++)
+						for(int b = 0; b < split[a].length; b++)
 						{
-							currentFile = new File(splitPath + "/" + split[b + (4 * a)]);
+							currentFile = new File(splitPath + "/" + split[a][b]);
 							currentFile.createNewFile();
 
 							FileChannel rwChannel = new RandomAccessFile(currentFile, "rw").getChannel();
@@ -144,41 +156,50 @@ class CombinedToSplit implements ActionListener
 	}
 
 	@SuppressWarnings("resource")
-	public byte[][] splitDataFile(String path) throws IOException
+	public byte[][] splitDataFile(String path, int splitAmt) throws IOException
 	{	
 		File file = new File(path);
 		FileChannel rwChannel = new RandomAccessFile(file, "rw").getChannel();
 		ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, file.length());
 
-		byte[][] fileDivide = new byte[4][(int) (file.length() / 4)];
+		byte[][] fileDivide = new byte[splitAmt][(int) (file.length() / splitAmt)];
 		byte[] wholeFile = new byte[(int) file.length()];
 
 		wrBuf.get(wholeFile);
 
 		for(int a = 0; a < wholeFile.length; a++)
-			fileDivide[a % 4][a / 4] = wholeFile[a];
+			fileDivide[a % splitAmt][a / splitAmt] = wholeFile[a];
 
 		rwChannel.close();
 		return fileDivide;
 	}
 
 	@SuppressWarnings("resource")
-	public byte[][] splitUserFile(String path) throws IOException
+	public byte[][] splitUserFile(String path, int splitAmt) throws IOException
 	{	
 		File file = new File(path);
 		FileChannel rwChannel = new RandomAccessFile(file, "rw").getChannel();
 		ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, file.length());
 
-		byte[][] fileDivide = new byte[4][(int) (file.length() / 4)];
+		byte[][] fileDivide = new byte[splitAmt][(int) (file.length() / splitAmt)];
 		byte[] wholeFile = new byte[(int) file.length()];
 
 		wrBuf.get(wholeFile);
 
-		for(int a = 0; a < wholeFile.length / 2; a++)
-			fileDivide[a % 2][a / 2] = wholeFile[a];
+		if(splitAmt == 2)
+		{
+			for(int a = 0; a < wholeFile.length; a++)
+				fileDivide[a % splitAmt][a / splitAmt] = wholeFile[a];
+		}
 
-		for(int b = wholeFile.length / 2; b < wholeFile.length; b++)
-			fileDivide[((b - wholeFile.length / 2) % 2) + 2][(b - wholeFile.length / 2) / 2] = wholeFile[b];
+		else
+		{
+			for(int a = 0; a < wholeFile.length / 2; a++)
+				fileDivide[a % 2][a / 2] = wholeFile[a];
+
+			for(int b = wholeFile.length / 2; b < wholeFile.length; b++)
+				fileDivide[((b - wholeFile.length / 2) % 2) + 2][(b - wholeFile.length / 2) / 2] = wholeFile[b];			
+		}
 
 		rwChannel.close();
 		return fileDivide;
@@ -187,17 +208,29 @@ class CombinedToSplit implements ActionListener
 
 class SplitToCombined implements ActionListener
 {
-	final String[] gamesSupported = {"JoJo's HFTF", "Third Strike"};
+	final String[] gamesSupported = {"JoJo's HFTF", "JoJo's Venture", "Third Strike", "Second Impact", "New Generation", "Red Earth"};
 
-	final String[][] combinedSet = {{"10", "20", "30", "31", "40", "41", "50", "51"}, //JoJo's HFTF
-			{"10", "20", "30", "31", "40", "41", "50", "51", "60", "61"}}; //Third Strike
+	final String[][] combinedSet = {
+			{"10", "20", "30", "31", "40", "41", "50", "51"}, //JoJo's HFTF
+			{"10", "20", "30", "31", "40", "41", "50"}, //JoJo's Venture
+			{"10", "20", "30", "31", "40", "41", "50", "51", "60", "61"}, //Third Strike
+			{"10", "20", "30", "31", "40", "41", "50", "51"}, //Second Impact
+			{"10", "30", "31", "40", "41", "50"}, //New Generation
+			{"10", "30", "31", "40", "41", "50"} //Red Earth
+	};
 
-	final String[][] splitSet = {{"jojoba-simm1.0", "jojoba-simm1.1", "jojoba-simm1.2", "jojoba-simm1.3", "jojoba-simm2.0", "jojoba-simm2.1", "jojoba-simm2.2", "jojoba-simm2.3", "jojoba-simm3.0", "jojoba-simm3.1", "jojoba-simm3.2", "jojoba-simm3.3", "jojoba-simm3.4", "jojoba-simm3.5", "jojoba-simm3.6", "jojoba-simm3.7", "jojoba-simm4.0", "jojoba-simm4.1", "jojoba-simm4.2", "jojoba-simm4.3", "jojoba-simm4.4", "jojoba-simm4.5", "jojoba-simm4.6", "jojoba-simm4.7", "jojoba-simm5.0", "jojoba-simm5.1", "jojoba-simm5.2", "jojoba-simm5.3", "jojoba-simm5.4", "jojoba-simm5.5", "jojoba-simm5.6", "jojoba-simm5.7"}, //JoJo's HFTF
-			{"sfiii3-simm1.0", "sfiii3-simm1.1", "sfiii3-simm1.2", "sfiii3-simm1.3", "sfiii3-simm2.0", "sfiii3-simm2.1", "sfiii3-simm2.2", "sfiii3-simm2.3", "sfiii3-simm3.0", "sfiii3-simm3.1", "sfiii3-simm3.2", "sfiii3-simm3.3", "sfiii3-simm3.4", "sfiii3-simm3.5", "sfiii3-simm3.6", "sfiii3-simm3.7", "sfiii3-simm4.0", "sfiii3-simm4.1", "sfiii3-simm4.2", "sfiii3-simm4.3", "sfiii3-simm4.4", "sfiii3-simm4.5", "sfiii3-simm4.6", "sfiii3-simm4.7", "sfiii3-simm5.0", "sfiii3-simm5.1", "sfiii3-simm5.2", "sfiii3-simm5.3", "sfiii3-simm5.4", "sfiii3-simm5.5", "sfiii3-simm5.6", "sfiii3-simm5.7", "sfiii3-simm6.0", "sfiii3-simm6.1", "sfiii3-simm6.2", "sfiii3-simm6.3", "sfiii3-simm6.4", "sfiii3-simm6.5", "sfiii3-simm6.6", "sfiii3-simm6.7"}}; //Third Strike
+	final String[][][] splitSet = {
+			{{"jojoba-simm1.0", "jojoba-simm1.1", "jojoba-simm1.2", "jojoba-simm1.3"}, {"jojoba-simm2.0", "jojoba-simm2.1", "jojoba-simm2.2", "jojoba-simm2.3"}, {"jojoba-simm3.0", "jojoba-simm3.1", "jojoba-simm3.2", "jojoba-simm3.3"}, {"jojoba-simm3.4", "jojoba-simm3.5", "jojoba-simm3.6", "jojoba-simm3.7"}, {"jojoba-simm4.0", "jojoba-simm4.1", "jojoba-simm4.2", "jojoba-simm4.3"}, {"jojoba-simm4.4", "jojoba-simm4.5", "jojoba-simm4.6", "jojoba-simm4.7"}, {"jojoba-simm5.0", "jojoba-simm5.1", "jojoba-simm5.2", "jojoba-simm5.3"}, {"jojoba-simm5.4", "jojoba-simm5.5", "jojoba-simm5.6", "jojoba-simm5.7"}}, //JoJo's HFTF
+			{{"jojo-simm-1.0", "jojo-simm-1.1", "jojo-simm-1.2", "jojo-simm-1.3"}, {"jojo-simm-2.0", "jojo-simm-2.1", "jojo-simm-2.2", "jojo-simm-2.3"}, {"jojo-simm-3.0", "jojo-simm-3.1", "jojo-simm-3.2", "jojo-simm-3.3"}, {"jojo-simm-3.4", "jojo-simm-3.5", "jojo-simm-3.6", "jojo-simm-3.7"}, {"jojo-simm-4.0", "jojo-simm-4.1", "jojo-simm-4.2", "jojo-simm-4.3"}, {"jojo-simm-4.4", "jojo-simm-4.5", "jojo-simm-4.6", "jojo-simm-4.7"}, {"jojo-simm-5.0", "jojo-simm-5.1"}}, //JoJo's Venture
+			{{"sfiii3-simm1.0", "sfiii3-simm1.1", "sfiii3-simm1.2", "sfiii3-simm1.3"}, {"sfiii3-simm2.0", "sfiii3-simm2.1", "sfiii3-simm2.2", "sfiii3-simm2.3"}, {"sfiii3-simm3.0", "sfiii3-simm3.1", "sfiii3-simm3.2", "sfiii3-simm3.3"}, {"sfiii3-simm3.4", "sfiii3-simm3.5", "sfiii3-simm3.6", "sfiii3-simm3.7"}, {"sfiii3-simm4.0", "sfiii3-simm4.1", "sfiii3-simm4.2", "sfiii3-simm4.3"}, {"sfiii3-simm4.4", "sfiii3-simm4.5", "sfiii3-simm4.6", "sfiii3-simm4.7"}, {"sfiii3-simm5.0", "sfiii3-simm5.1", "sfiii3-simm5.2", "sfiii3-simm5.3"}, {"sfiii3-simm5.4", "sfiii3-simm5.5", "sfiii3-simm5.6", "sfiii3-simm5.7"}, {"sfiii3-simm6.0", "sfiii3-simm6.1", "sfiii3-simm6.2", "sfiii3-simm6.3"}, {"sfiii3-simm6.4", "sfiii3-simm6.5", "sfiii3-simm6.6", "sfiii3-simm6.7"}}, //Third Strike
+			{{"sfiii2-simm1.0", "sfiii2-simm1.1", "sfiii2-simm1.2", "sfiii2-simm1.3"}, {"sfiii2-simm2.0", "sfiii2-simm2.1", "sfiii2-simm2.2", "sfiii2-simm2.3"}, {"sfiii2-simm3.0", "sfiii2-simm3.1", "sfiii2-simm3.2", "sfiii2-simm3.3"}, {"sfiii2-simm3.4", "sfiii2-simm3.5", "sfiii2-simm3.6", "sfiii2-simm3.7"}, {"sfiii2-simm4.0", "sfiii2-simm4.1", "sfiii2-simm4.2", "sfiii2-simm4.3"}, {"sfiii2-simm4.4", "sfiii2-simm4.5", "sfiii2-simm4.6", "sfiii2-simm4.7"}, {"sfiii2-simm5.0", "sfiii2-simm5.1", "sfiii2-simm5.2", "sfiii2-simm5.3"}, {"sfiii2-simm5.4", "sfiii2-simm5.5", "sfiii2-simm5.6", "sfiii2-simm5.7"}}, //Second Impact
+			{{"sfiii-simm1.0", "sfiii-simm1.1", "sfiii-simm1.2", "sfiii-simm1.3"}, {"sfiii-simm3.0", "sfiii-simm3.1", "sfiii-simm3.2", "sfiii-simm3.3"}, {"sfiii-simm3.4", "sfiii-simm3.5", "sfiii-simm3.6", "sfiii-simm3.7"}, {"sfiii-simm4.0", "sfiii-simm4.1", "sfiii-simm4.2", "sfiii-simm4.3"}, {"sfiii-simm4.4", "sfiii-simm4.5", "sfiii-simm4.6", "sfiii-simm4.7"}, {"sfiii-simm5.0", "sfiii-simm5.1"}}, //New Generation
+			{{"redearth-simm1.0", "redearth-simm1.1", "redearth-simm1.2", "redearth-simm1.3"}, {"redearth-simm3.0", "redearth-simm3.1", "redearth-simm3.2", "redearth-simm3.3"}, {"redearth-simm3.4", "redearth-simm3.5", "redearth-simm3.6", "redearth-simm3.7"}, {"redearth-simm4.0", "redearth-simm4.1", "redearth-simm4.2", "redearth-simm4.3"}, {"redearth-simm4.4", "redearth-simm4.5", "redearth-simm4.6", "redearth-simm4.7"}, {"redearth-simm5.0", "redearth-simm5.1"}} //Red Earth
+	};
 
 	String[] combined;
-	String[] split;
-	
+	String[][] split;
+
 	static JFileChooser chooser = new JFileChooser();
 
 	@SuppressWarnings("resource")
@@ -205,13 +238,10 @@ class SplitToCombined implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		int gameSelected = JOptionPane.showOptionDialog(null, "Please choose the game you wish to convert from split to combined", "Select a Game", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, gamesSupported, gamesSupported[0]);
-		
-		for(String str : splitSet[gameSelected])
-			System.out.println(str);
-		
+
 		combined = combinedSet[gameSelected];
 		split = splitSet[gameSelected];
-		
+
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		JOptionPane.showMessageDialog(null, "Please select the directory where the Split ROM is located");
 
@@ -227,8 +257,6 @@ class SplitToCombined implements ActionListener
 
 			if(returnVal == JFileChooser.APPROVE_OPTION)
 			{
-
-
 				String combinedPath = chooser.getSelectedFile().toString();
 				byte[] wholeFile = null;
 				File currentFile = null;
@@ -238,10 +266,10 @@ class SplitToCombined implements ActionListener
 					try 
 					{
 						if(a < 2)
-							wholeFile = combineDataFile(splitPath, a * 4);
+							wholeFile = combineDataFile(splitPath, a, split[a].length);
 
 						else
-							wholeFile = combineUserFile(splitPath, a * 4);
+							wholeFile = combineUserFile(splitPath, a, split[a].length);
 
 						currentFile = new File(combinedPath + "/" + combined[a]);
 						currentFile.createNewFile();
@@ -264,12 +292,12 @@ class SplitToCombined implements ActionListener
 	}
 
 	@SuppressWarnings("resource")
-	public byte[] combineDataFile(String path, int start) throws IOException
+	public byte[] combineDataFile(String path, int start, int splitAmt) throws IOException
 	{	
-		File file = new File(path + "/" + split[start]);
-		byte[] wholeFile = new byte[(int) file.length() * 4];
+		File file = new File(path + "/" + split[start][0]);
+		byte[] wholeFile = new byte[(int) file.length() * splitAmt];
 
-		for(int a = 0; a < 4; a++)
+		for(int a = 0; a < splitAmt; a++)
 		{
 			file = new File(path + "/" + split[start + a]);
 			byte[] splitFile = new byte[(int) file.length()];
@@ -280,7 +308,7 @@ class SplitToCombined implements ActionListener
 			wrBuf.get(splitFile);
 
 			for(int b = 0; b < splitFile.length; b++)
-				wholeFile[a + (b * 4)] = splitFile[b];
+				wholeFile[a + (b * splitAmt)] = splitFile[b];
 
 			rwChannel.close();
 		}
@@ -289,10 +317,10 @@ class SplitToCombined implements ActionListener
 	}
 
 	@SuppressWarnings("resource")
-	public byte[] combineUserFile(String path, int start) throws IOException
+	public byte[] combineUserFile(String path, int start, int splitAmt) throws IOException
 	{	
-		File file = new File(path + "/" + split[start]);
-		byte[] wholeFile = new byte[(int) file.length() * 4];
+		File file = new File(path + "/" + split[start][0]);
+		byte[] wholeFile = new byte[(int) file.length() * splitAmt];
 
 		for(int a = 0; a < 2; a++)
 		{
@@ -310,20 +338,23 @@ class SplitToCombined implements ActionListener
 			rwChannel.close();
 		}
 
-		for(int a = 2; a < 4; a++)
+		if(splitAmt == 4)
 		{
-			file = new File(path + "/" + split[start + a]);
-			byte[] splitFile = new byte[(int) file.length()];
+			for(int a = 2; a < 4; a++)
+			{
+				file = new File(path + "/" + split[start + a]);
+				byte[] splitFile = new byte[(int) file.length()];
 
-			FileChannel rwChannel = new RandomAccessFile(file, "rw").getChannel();
-			ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, file.length());
+				FileChannel rwChannel = new RandomAccessFile(file, "rw").getChannel();
+				ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, file.length());
 
-			wrBuf.get(splitFile);
+				wrBuf.get(splitFile);
 
-			for(int b = 0; b < splitFile.length; b++)
-				wholeFile[(wholeFile.length / 2) + (a - 2) + (b * 2)] = splitFile[b];
+				for(int b = 0; b < splitFile.length; b++)
+					wholeFile[(wholeFile.length / 2) + (a - 2) + (b * 2)] = splitFile[b];
 
-			rwChannel.close();
+				rwChannel.close();
+			}
 		}
 
 		return wholeFile;
